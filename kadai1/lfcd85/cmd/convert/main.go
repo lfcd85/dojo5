c252542e9a70e25853da43995c6d9dd7dad37e47
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-05-29 08:56:16 +0900

    Add imgconv package

--- kadai1/lfcd85/cmd/convert/main.go
+++ kadai1/lfcd85/cmd/convert/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/lfcd85/dojo5/kadai1/lfcd85/imgconv"
+)
 
 func main() {
 	fmt.Println("Hello, gopherdojo!")
+	imgconv.Convert()
 }
---
+++ kadai1/lfcd85/imgconv/imgconv.go
@@ -0,0 +1,7 @@
+package imgconv
+
+import "fmt"
+
+func Convert() {
+	fmt.Println("This func is going to convert the images.")
+}
