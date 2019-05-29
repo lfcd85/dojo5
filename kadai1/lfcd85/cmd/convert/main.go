ea63be4fd7bcd79723413674f6708bc7d4157521
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-05-29 10:27:05 +0900

    Enable to convert single JPEG -> PNG file

--- kadai1/lfcd85/cmd/convert/main.go
+++ kadai1/lfcd85/cmd/convert/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/lfcd85/dojo5/kadai1/lfcd85/imgconv"
 )
 
 func main() {
-	fmt.Println("Hello, gopherdojo!")
-	imgconv.Convert()
+	imgconv.Convert("./test/sample1.jpg")
 }
--- kadai1/lfcd85/imgconv/imgconv.go
+++ kadai1/lfcd85/imgconv/imgconv.go
@@ -1,7 +1,35 @@
 package imgconv
 
-import "fmt"
+import (
+	"image"
+	"image/png"
+	"os"
 
-func Convert() {
-	fmt.Println("This func is going to convert the images.")
+	_ "image/jpeg"
+)
+
+func Convert(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+	writeOutputFile(img)
+}
+
+func writeOutputFile(img image.Image) {
+	file, err := os.Create("output.png")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	if err := png.Encode(file, img); err != nil {
+		panic(err)
+	}
 }
